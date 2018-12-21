00e2f3594734f30b8be08f8441d9213c588a518e
felix <felix@psy-coding.com>
2018-12-21 10:19:19 +0800

    add basic testing for config handling

---
+++ config_test.go
@@ -0,0 +1,135 @@
+package cagent
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/troian/toml"
+)
+
+func TestNewMinimumConfig(t *testing.T) {
+	envURL := "http://foo.bar"
+	envUser := "foo"
+	envPass := "bar"
+
+	// TODO: Not sure if this is really a good idea... could mess with other things
+	os.Setenv("CAGENT_HUB_URL", envURL)
+	os.Setenv("CAGENT_HUB_USER", envUser)
+	os.Setenv("CAGENT_HUB_PASSWORD", envPass)
+
+	mvc := NewMinimumConfig()
+
+	assert.Equal(t, envURL, mvc.HubURL, "HubURL should be set from env")
+	assert.Equal(t, envUser, mvc.HubUser, "HubUser should be set from env")
+	assert.Equal(t, envPass, mvc.HubPassword, "HubPassword should be set from env")
+
+	// Unset in the end for cleanup
+	defer os.Clearenv()
+}
+
+func TestTryUpdateConfigFromFile(t *testing.T) {
+	config := Config{
+		PidFile:   "fooPID",
+		Interval:  1.5,
+		HubGzip:   false,
+		FSMetrics: []string{"a"},
+	}
+
+	const sampleConfig = `
+pid = "/pid"
+interval = 1.0
+hub_gzip = true
+fs_metrics = ['a', 'b']
+`
+
+	tmpFile, err := ioutil.TempFile("", "")
+	assert.Nil(t, err)
+	defer os.Remove(tmpFile.Name())
+
+	err = ioutil.WriteFile(tmpFile.Name(), []byte(sampleConfig), 0755)
+	assert.Nil(t, err)
+
+	err = TryUpdateConfigFromFile(&config, tmpFile.Name())
+	assert.Nil(t, err)
+
+	assert.Equal(t, "/pid", config.PidFile)
+	assert.Equal(t, 1.0, config.Interval)
+	assert.Equal(t, true, config.HubGzip)
+	assert.Equal(t, []string{"a", "b"}, config.FSMetrics)
+}
+
+func TestGenerateDefaultConfigFile(t *testing.T) {
+	mvc := &MinValuableConfig{
+		LogLevel: "debug",
+		HubUser:  "bar",
+	}
+
+	tmpFile, err := ioutil.TempFile("", "")
+	assert.Nil(t, err)
+	defer os.Remove(tmpFile.Name())
+
+	err = GenerateDefaultConfigFile(mvc, tmpFile.Name())
+	assert.Nil(t, err)
+
+	loadedMVC := &MinValuableConfig{}
+	_, err = toml.DecodeReader(tmpFile, loadedMVC)
+	assert.Nil(t, err)
+
+	if !assert.ObjectsAreEqual(*mvc, *loadedMVC) {
+		t.Errorf("expected %+v, got %+v", *mvc, *loadedMVC)
+	}
+}
+
+func TestHandleAllConfigSetup(t *testing.T) {
+	t.Run("config-file-does-exist", func(t *testing.T) {
+		const sampleConfig = `
+pid = "/pid"
+interval = 1.0
+hub_gzip = true
+fs_metrics = ['a', 'b']
+`
+
+		tmpFile, err := ioutil.TempFile("", "")
+		assert.Nil(t, err)
+		defer os.Remove(tmpFile.Name())
+
+		err = ioutil.WriteFile(tmpFile.Name(), []byte(sampleConfig), 0755)
+		assert.Nil(t, err)
+
+		config, err := HandleAllConfigSetup(tmpFile.Name())
+		assert.Nil(t, err)
+
+		assert.Equal(t, "/pid", config.PidFile)
+		assert.Equal(t, 1.0, config.Interval)
+		assert.Equal(t, true, config.HubGzip)
+		assert.Equal(t, []string{"a", "b"}, config.FSMetrics)
+	})
+
+	t.Run("config-file-does-not-exist", func(t *testing.T) {
+		// Create a temp file to get a file path we can use for temp
+		// config generation. But delete it so we can acutally write our
+		// config file under the path.
+		tmpFile, err := ioutil.TempFile("", "")
+		assert.Nil(t, err)
+		configFilePath := tmpFile.Name()
+		err = os.Remove(tmpFile.Name())
+		assert.Nil(t, err)
+
+		_, err = HandleAllConfigSetup(configFilePath)
+		assert.Nil(t, err)
+
+		_, err = os.Stat(configFilePath)
+		assert.Nil(t, err)
+
+		mvc := NewMinimumConfig()
+		loadedMVC := &MinValuableConfig{}
+		_, err = toml.DecodeFile(configFilePath, loadedMVC)
+		assert.Nil(t, err)
+
+		if !assert.ObjectsAreEqual(*mvc, *loadedMVC) {
+			t.Errorf("expected %+v, got %+v", *mvc, *loadedMVC)
+		}
+	})
+}
