b44ecd8706e3215fb7df2222bcd6b15e973aea2e
Artur Troian <troian.ap@gmail.com>
2019-01-16 18:24:42 +0200

    doc: mention plesk depends on cagent config path
    Signed-off-by: Artur Troian <troian.ap@gmail.com>

--- pkg-scripts/postinstall.sh
+++ pkg-scripts/postinstall.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+
+# WARN: path to config is referenced by plesk extension
+#       when enabling/disabling plugin thus plesk extension
+#       has to be updated prior to cagent
 /usr/bin/cagent -s cagent -c /etc/cagent/cagent.conf
 /usr/bin/cagent -t || true
