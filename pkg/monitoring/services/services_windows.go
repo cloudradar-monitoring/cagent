d88d210b3ecfad4e55212454e8009a3e297b0573
requilence <requilence@gmail.com>
2019-01-28 12:44:45 +0200

    Improve services list
    - show only autostarting service by default(discover_autostarting_services_only in config)
    - use `state` field for all of system managers to make filtering in the UI easier
    - query `delayed autostart` status on win

--- pkg/monitoring/services/services_windows.go
+++ pkg/monitoring/services/services_windows.go
@@ -4,8 +4,10 @@ package services
 
 import (
 	"context"
-	"github.com/StackExchange/wmi"
+	"strings"
 	"time"
+
+	"github.com/StackExchange/wmi"
 )
 
 const serviceListTimeout = time.Second * 5
@@ -14,11 +16,13 @@ const serviceListTimeout = time.Second * 5
 var ErrorNotImplementedForOS error
 
 type Win32_Service struct {
-	Name        string
-	DisplayName string
-	StartMode   string
-	State       string
-	Status      string
+	Name             string
+	DisplayName      string
+	Description      string
+	StartMode        string
+	State            string
+	Status           string
+	DelayedAutoStart bool
 }
 
 // todo: move to the separate package when we will also move processes.go to the separate package
@@ -37,25 +41,40 @@ func wmiQueryWithContext(ctx context.Context, query string, dst interface{}, con
 }
 
 // ListServices parse Windows Service Manager
-func ListServices() (map[string]interface{}, error) {
+func ListServices(autostartOnly bool) (map[string]interface{}, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), serviceListTimeout)
 	defer cancel()
 
 	var wmiServices []Win32_Service
-	err := wmiQueryWithContext(ctx, "Select Name,DisplayName,StartMode,State,Status from Win32_Service", &wmiServices)
+	err := wmiQueryWithContext(ctx, "Select Name,DisplayName,Description,StartMode,State,State,DelayedAutoStart from Win32_Service", &wmiServices)
 	if err != nil {
 		return nil, err
 	}
 
-	var servicesList []map[string]string
+	var servicesList []map[string]interface{}
 	for _, wmiService := range wmiServices {
-		servicesList = append(servicesList, map[string]string{
-			"name":         wmiService.Name,
-			"display_name": wmiService.DisplayName,
-			"start":        wmiService.StartMode,
-			"state":        wmiService.State,
-			"status":       wmiService.Status,
-			"manager":      "windows",
+		var autoStart bool
+
+		if strings.HasPrefix(strings.ToLower(wmiService.StartMode), "auto") {
+			autoStart = true
+		}
+
+		if autostartOnly && !autoStart {
+			continue
+		}
+
+		if wmiService.DelayedAutoStart {
+			wmiService.StartMode = wmiService.StartMode + "_delayed"
+		}
+
+		servicesList = append(servicesList, map[string]interface{}{
+			"name":        wmiService.Name,
+			"description": wmiService.DisplayName + " " + wmiService.Description,
+			"start":       strings.ToLower(wmiService.StartMode),
+			"auto_start":  autoStart,
+			"state":       strings.ToLower(wmiService.State),
+			"status":      strings.ToLower(wmiService.Status),
+			"manager":     "windows",
 		})
 	}
 
