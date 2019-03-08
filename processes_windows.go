608c772bb1ce7658149f3320c8270b9c238d53f4
nikita-vanyasin <nikita.vanyasin@gmail.com>
2019-03-08 14:20:27 +0300

    Eliminate WMI call when querying for the list of processes

--- processes_windows.go
+++ processes_windows.go
@@ -3,78 +3,33 @@
 package cagent
 
 import (
-	"context"
-	"errors"
-	"time"
-
-	"github.com/StackExchange/wmi"
-
 	"github.com/cloudradar-monitoring/cagent/pkg/monitoring/docker"
+	"github.com/cloudradar-monitoring/cagent/pkg/winapi"
+	"github.com/pkg/errors"
 )
 
-const processListTimeout = time.Second * 10
-
-type Win32_Process struct {
-	Name            *string
-	CommandLine     *string
-	ProcessID       *uint32
-	ParentProcessId *uint32
-	ExecutionState  *uint16
-}
-
-func WMIQueryWithContext(ctx context.Context, query string, dst interface{}, connectServerArgs ...interface{}) error {
-	errChan := make(chan error, 1)
-	go func() {
-		errChan <- wmi.Query(query, dst, connectServerArgs...)
-	}()
-
-	select {
-	case <-ctx.Done():
-		return ctx.Err()
-	case err := <-errChan:
-		return err
-	}
-}
-
 func processes(_ *docker.Watcher) ([]ProcStat, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), processListTimeout)
-	defer cancel()
-
-	wmiProcs := []Win32_Process{}
-
-	err := WMIQueryWithContext(ctx, `SELECT Name, CommandLine, ProcessID, ParentProcessId FROM Win32_Process`, &wmiProcs)
+	procs, err := winapi.GetSystemProcessInformation()
 	if err != nil {
-		if err == context.DeadlineExceeded {
-			return nil, TimeoutError{"WMI query", processListTimeout}
-		}
-		return nil, errors.New("WMI query error: " + err.Error())
+		return nil, errors.Wrap(err, "[PROC] can't get system processes")
 	}
 
-	var procs []ProcStat
-	for _, proc := range wmiProcs {
-		if proc.ProcessID == nil {
+	var result []ProcStat
+	for pid, proc := range procs {
+		if pid == 0 {
 			continue
 		}
 
 		ps := ProcStat{
-			PID:   int(*proc.ProcessID),
-			State: "running",
+			PID:       int(pid),
+			ParentPID: int(proc.InheritedFromUniqueProcessId),
+			State:     "running",
+			Name:      proc.ImageName.String(),
+			Cmdline:   "",
 		}
 
-		if proc.Name != nil {
-			ps.Name = *proc.Name
-		}
-
-		if proc.ParentProcessId != nil {
-			ps.ParentPID = int(*proc.ParentProcessId)
-		}
-
-		if proc.CommandLine != nil {
-			ps.Cmdline = *proc.CommandLine
-		}
-
-		procs = append(procs, ps)
+		result = append(result, ps)
 	}
 
-	return procs, nil
+	return result, nil
 }
