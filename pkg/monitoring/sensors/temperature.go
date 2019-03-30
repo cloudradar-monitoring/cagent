32f6ab4bbf1d066f5fd338c50fba2eef769442e7
nikita-vanyasin <nikita.vanyasin@gmail.com>
2019-03-30 11:04:27 +0300

    Report temperature sensor values

---
+++ pkg/monitoring/sensors/temperature.go
@@ -0,0 +1,47 @@
+package sensors
+
+import (
+	"context"
+	"strings"
+	"time"
+
+	"github.com/shirou/gopsutil/host"
+	"github.com/sirupsen/logrus"
+)
+
+const readTimeout = time.Second * 10
+const unitCelsius = "centigrade"
+
+type TemperatureSensorInfo struct {
+	SensorName  string  `json:"sensor_name"`
+	Temperature float64 `json:"temperature"`
+	Unit        string  `json:"unit"`
+}
+
+func ReadTemperatureSensors() ([]*TemperatureSensorInfo, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), readTimeout)
+	defer cancel()
+
+	sensors, err := host.SensorsTemperaturesWithContext(ctx)
+	if err != nil {
+		errText := strings.ToLower(err.Error())
+		if strings.Contains(errText, "not implemented") {
+			logrus.Info("[SENSORS] reading temperature sensors is not implemented for this OS")
+		} else if strings.Contains(errText, "not supported") {
+			logrus.WithError(err).Debugf("[SENSORS] not supported by BIOS or driver required")
+		} else {
+			logrus.WithError(err).Error("[SENSORS] failed to read temperature sensors")
+		}
+		return nil, nil
+	}
+
+	result := make([]*TemperatureSensorInfo, 0)
+	for _, sensor := range sensors {
+		result = append(result, &TemperatureSensorInfo{
+			SensorName:  sensor.SensorKey,
+			Temperature: sensor.Temperature,
+			Unit:        unitCelsius,
+		})
+	}
+	return result, nil
+}
