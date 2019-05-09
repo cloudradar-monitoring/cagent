3782fb4b2c00753f4b8aab72b5ba8a14bdf352b1
Artur Troian <troian.ap@gmail.com>
2019-05-09 22:36:34 +0300

    fix: SMART version test
    Signed-off-by: Artur Troian <troian.ap@gmail.com>

--- pkg/smart/smartctl_version_test.go
+++ pkg/smart/smartctl_version_test.go
@@ -3,31 +3,23 @@ package smart
 import (
 	"testing"
 
-	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestSmartctlVersionValid(t *testing.T) {
 	ver := "smartctl 7.0 2018-12-30 r4883 [Darwin 18.2.0 x86_64] (local build)"
 
-	parsedVer, err := smartctlIsSupportedVersion(ver)
+	major, minor, err := smartctlParseVersion(ver)
 	assert.NoError(t, err)
-	assert.Equal(t, "7.0", parsedVer)
+	assert.Equal(t, atLeastMajorVersion, major)
+	assert.Equal(t, 0, minor)
 }
 
 func TestSmartctlVersionInvalid1(t *testing.T) {
 	ver := ""
 
-	parsedVer, err := smartctlIsSupportedVersion(ver)
+	major, minor, err := smartctlParseVersion(ver)
 	assert.Error(t, err)
-	assert.Equal(t, "", parsedVer)
-}
-
-func TestSmartctlVersionInvalid2(t *testing.T) {
-	ver := "smartctl 6.5 2018-12-30 r4883 [Darwin 18.2.0 x86_64] (local build)"
-
-	parsedVer, err := smartctlIsSupportedVersion(ver)
-	assert.Error(t, err)
-	assert.EqualError(t, errors.New("smart: unsupported smartctl version. expected minimum [7.0], actual [6.5]"), err.Error())
-	assert.Equal(t, "", parsedVer)
+	assert.Equal(t, 0, major)
+	assert.Equal(t, 0, minor)
 }
