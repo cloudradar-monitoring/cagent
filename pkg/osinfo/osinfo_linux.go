5b00df720109889bef6a4b5427112368aaa4e70c
Artur Troian <troian.ap@gmail.com>
2019-05-26 22:08:39 +0300

    fix: pretty format os name for debian distributives
    Signed-off-by: Artur Troian <troian.ap@gmail.com>

--- pkg/osinfo/osinfo_linux.go
+++ pkg/osinfo/osinfo_linux.go
@@ -3,15 +3,21 @@
 package osinfo
 
 import (
+	"bytes"
+	"fmt"
 	"io/ioutil"
 	"os"
+	"regexp"
 	"strings"
 
 	"github.com/pkg/errors"
+	log "github.com/sirupsen/logrus"
+	"golang.org/x/sys/unix"
 )
 
 var releaseFiles = map[string][]string{
 	"Novell SUSE":   {"/etc/SUSE-release"},
+	"CentOS":        {"/etc/centos-release"},
 	"Red Hat":       {"/etc/redhat-release", "/etc/redhat_version"},
 	"Fedora":        {"/etc/fedora-release"},
 	"Slackware":     {"/etc/slackware-release", "/etc/slackware-version"},
@@ -25,7 +31,24 @@ var releaseFiles = map[string][]string{
 	"Ubuntu":        {"/etc/lsb-release"},
 }
 
+var ubuntuDescriptionRegexp = regexp.MustCompile(`(?m)^DISTRIB_DESCRIPTION=\"(.+)\"$`)
+var ubuntuCodenameRegexp = regexp.MustCompile(`(?m)^DISTRIB_CODENAME=(\w+)$`)
+
+func uname() (*unix.Utsname, error) {
+	uts := &unix.Utsname{}
+
+	if err := unix.Uname(uts); err != nil {
+		return nil, err
+	}
+	return uts, nil
+}
+
 func osName() (string, error) {
+	uts, err := uname()
+	if err != nil {
+		log.WithError(err)
+	}
+
 	for dist, files := range releaseFiles {
 		if _, err := os.Stat(files[0]); err == nil {
 			result := dist
@@ -34,16 +57,36 @@ func osName() (string, error) {
 				return result, errors.Wrapf(err, "osinfo: couldn't read release info from \"%s\"", files[0])
 			}
 
-			result += ": " + strings.TrimSuffix(string(data), "\n")
-			result = strings.TrimSuffix(result, " ")
-			result = strings.ReplaceAll(result, "\n", " ")
-			if len(files) == 2 {
-				if _, err = os.Stat(files[1]); err == nil {
-					if data, err = ioutil.ReadFile(files[1]); err != nil {
-						return result, errors.Wrapf(err, "osinfo: couldn't read version info from \"%s\"", files[0])
-					}
-					result += ": " + strings.TrimSuffix(string(data), "\n")
+			switch dist {
+			case "Ubuntu":
+				fallthrough
+			case "Debian":
+				if ubuntuDescriptionRegexp.Match(data) {
+					res := ubuntuDescriptionRegexp.FindAllStringSubmatch(string(data), -1)
+					result += ": " + res[0][1]
 				}
+
+				if ubuntuCodenameRegexp.Match(data) {
+					res := ubuntuCodenameRegexp.FindAllStringSubmatch(string(data), -1)
+					result += " (" + res[0][1] + ")"
+				}
+
+				if uts != nil {
+					result += fmt.Sprintf(" %s", string(uts.Release[:bytes.IndexByte(uts.Release[:], 0)]))
+				}
+			default:
+				result += ": " + strings.TrimSuffix(string(data), "\n")
+				result = strings.TrimSuffix(result, " ")
+				result = strings.ReplaceAll(result, "\n", " ")
+				if len(files) == 2 {
+					if _, err = os.Stat(files[1]); err == nil {
+						if data, err = ioutil.ReadFile(files[1]); err != nil {
+							return result, errors.Wrapf(err, "osinfo: couldn't read version info from \"%s\"", files[0])
+						}
+						result += ": " + strings.TrimSuffix(string(data), "\n")
+					}
+				}
+
 			}
 
 			return result, nil
