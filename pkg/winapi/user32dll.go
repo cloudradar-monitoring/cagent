677583c5d3993312f66dd97c91ecc0e69dd5478f
Nikita Vanyasin <nikita.vanyasin@gmail.com>
2020-04-04 20:51:30 +0300

    Fix panic "too many callbacks" when running agent for a long time on Windows
    syscall.NewCallback() may be called only 2000 times in Go runtime.
    Afterwards it throws a panic. The implementation updated to execute it only once.

--- pkg/winapi/user32dll.go
+++ pkg/winapi/user32dll.go
@@ -14,6 +14,44 @@ var (
 	procIsHungAppWindow          = user32.MustFindProc("IsHungAppWindow")
 )
 
+
+type WindowsEnumerator struct {
+	currentList        *map[uint32]syscall.Handle
+	compiledCallbackFn uintptr
+}
+
+// NewWindowsEnumerator returns an object for enumerating window handles
+// it's important not to create too many enumerators as
+// there is a runtime limit of how many callback objects can be created
+func NewWindowsEnumerator() *WindowsEnumerator {
+	list := make(map[uint32]syscall.Handle)
+	we := WindowsEnumerator{
+		currentList: &list,
+	}
+	we.compiledCallbackFn = syscall.NewCallback(func(h syscall.Handle, p uintptr) uintptr {
+		var windowProcessId uint32
+		_, err := GetWindowThreadProcessId(h, &windowProcessId)
+		if err != nil {
+			// ignore the error
+			return 1 // continue enumeration
+		}
+		list[windowProcessId] = h
+
+		return 1 // continue enumeration
+	})
+	return &we
+}
+
+func (we *WindowsEnumerator) Enumerate() (map[uint32]syscall.Handle, error) {
+	*we.currentList = make(map[uint32]syscall.Handle)
+	err := EnumWindows(we.compiledCallbackFn, 0)
+	if err != nil {
+		return nil, err
+	}
+	return *(we.currentList), nil
+}
+
+
 func EnumWindows(enumFunc uintptr, lparam uintptr) (err error) {
 	r1, _, e1 := syscall.Syscall(procEnumWindows.Addr(), 2, uintptr(enumFunc), uintptr(lparam), 0)
 	if r1 == 0 {
@@ -37,29 +75,6 @@ func GetWindowThreadProcessId(hwnd syscall.Handle, str *uint32) (len int32, err 
 		}
 	}
 	return
-}
-
-func WindowByProcessId() (map[uint32]syscall.Handle, error) {
-	m := make(map[uint32]syscall.Handle)
-
-	cb := syscall.NewCallback(func(h syscall.Handle, p uintptr) uintptr {
-		var windowProcessId uint32
-		_, err := GetWindowThreadProcessId(h, &windowProcessId)
-		if err != nil {
-			// ignore the error
-			return 1 // continue enumeration
-		}
-		m[windowProcessId] = h
-
-		return 1 // continue enumeration
-	})
-
-	err := EnumWindows(cb, 0)
-	if err != nil {
-		return nil, err
-	}
-
-	return m, nil
 }
 
 func IsHangWindow(hwnd syscall.Handle) (bool, error) {
